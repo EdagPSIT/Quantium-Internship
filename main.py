e1f7d69723a19c87d808eafc00a8f0a96fcd68b6
EdagPSIT <vhanamaneramesh@gmail.com>
2023-10-13 18:34:04 +0530

    Task 4 Completed

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc,Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
+external_stylesheets = ['style.css']
 
 # Building the dash application
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Reading the data into dataframe
 df = pd.read_csv('merged_sales_data.csv')
@@ -18,16 +19,42 @@ fig = px.line(df, x='date',
 
 app.layout = html.Div(children=[
     html.H1(children='Soul Foods - Pink Morsel Sales Analysis'),
+    # Radio Button
+    dcc.RadioItems(['North', 'East ','South','West','All'], 'All', inline=True,id='region-radio-items'),
 
-    html.Div(children='''
-        Pink Morsel Sales trend
-    '''),
+    html.Br(),
 
+    html.Div(id='line_fig_title'),
     dcc.Graph(
         id='sales_graph',
         figure=fig
     )
 ])
+
+
+@callback(
+    Output('sales_graph', 'figure'),
+    Output('line_fig_title','children'),
+    Input('region-radio-items', 'value')
+)
+def update_layout(selected_radio_item):
+    if selected_radio_item == 'All':
+        title = "Pink Morsel Sales - {} Regions".format(selected_radio_item)
+        fig = px.line(df, x='date',
+                      y='Sales',
+                      labels={'date': 'Dates', 'Sales': "Sales ($)"})
+        return fig,title
+    else:
+        dff = df[df['region'] == selected_radio_item.lower()]
+        title = "Pink Morsel Sales - {} Region".format(selected_radio_item)
+        fig = px.line(dff, x='date',
+                      y='Sales',
+                      labels={'date': 'Dates', 'Sales': "Sales ($)"})
+        return fig,title
+
+
+# Add the external CSS file
+app.css.append_css({'external_url': 'style.css'})
 
 if __name__ == '__main__':
     app.run(debug=True)
